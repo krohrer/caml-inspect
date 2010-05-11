1de65a58dce3dfc7d8e89fa256616ae589b45744
Kaspar M. Rohrer <kaspar.rohrer@gmail.com>
2010-05-11 13:39:38 +0200

    Cleaned out some unused stuff.

--- value_cimpl.c
+++ value_cimpl.c
@@ -5,14 +5,11 @@
 #include "caml/callback.h"
 #include "caml/custom.h"
 
-#define Is_custom(v) (Tag_val(v) == Custom_tag)
-#define Is_custom_id(v, id) (Tag_val(v) == Custom_tag && 0 == strcmp(Custom_ops_val(v)->identifier, cstr_id))
-
 CAMLexport value inspect_custom_id(value v)
 {
   CAMLparam1(v);
   CAMLlocal1(result);
-  if (Is_custom(v)) {
+  if (Tag_val(v) == Custom_tag) {
     result = caml_copy_string(Custom_ops_val(v)->identifier);
   }
   else {
@@ -29,13 +26,10 @@ CAMLexport value inspect_bits(value v)
 
 #define Inspect_custom_has(op)						\
   CAMLexport value inspect_custom_has_##op(value v)			\
-  { return Val_bool(Is_custom(v) && Custom_ops_val(v)->op != NULL); }
+  { return Val_bool(Tag_val(v) == Custom_tag && Custom_ops_val(v)->op != NULL); }
 
 Inspect_custom_has (finalize)
 Inspect_custom_has (compare)
 Inspect_custom_has (hash)
 Inspect_custom_has (serialize)
 Inspect_custom_has (deserialize)
-
-#undef Inspect_custom_has
-
